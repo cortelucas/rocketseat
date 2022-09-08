71e76dddc82f868aecab756c76f0daae755ddede
cortelucas <corte1994.lc@gmail.com>
2022-09-08 17:11:35 -0300

    feat: implement repository in memory

---
+++ lives/test-apps-node/src/repositories/in-memory/in-memory-appointments-ropository.ts
@@ -0,0 +1,27 @@
+import { areIntervalsOverlapping } from 'date-fns'
+import { Appointment } from '../../entities/appointment/appointment'
+import { AppointmentRepository } from '../appointments-ropository'
+
+export class InMemoryAppointmentRepository implements AppointmentRepository {
+  public appointments: Appointment[] = []
+
+  async create (appointment: Appointment): Promise<void> {
+    this.appointments.push(appointment)
+  }
+
+  async findOverlappingAppointment (startsAt: Date, endsAt: Date): Promise<Appointment | null> {
+    const overlappingAppointment = this.appointments.find(appointment => {
+      return areIntervalsOverlapping(
+        { start: startsAt, end: endsAt },
+        { start: appointment.startsAt, end: appointment.endsAt },
+        { inclusive: true }
+      )
+    })
+
+    if (!overlappingAppointment) {
+      return null
+    }
+
+    return overlappingAppointment
+  }
+}
