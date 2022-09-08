e21f353121818320346ae02b534f71707943c2ae
cortelucas <corte1994.lc@gmail.com>
2022-09-08 17:24:54 -0300

    test: add more tests

--- lives/test-apps-node/src/use-cases/appointment/create-appointment.test.ts
+++ lives/test-apps-node/src/use-cases/appointment/create-appointment.test.ts
@@ -1,11 +1,13 @@
 import { describe, expect, it } from 'vitest'
 import { Appointment } from '../../entities/appointment/appointment'
+import { InMemoryAppointmentRepository } from '../../repositories/in-memory/in-memory-appointments-repository'
 import { getFutureDate } from '../../tests/utils/get-future-date'
 import { CreateAppointment } from './create-appointment'
 
 describe('Create Appointment', () => {
   it('should be able to create an appointment', () => {
-    const sut = new CreateAppointment()
+    const appointmentRepository = new InMemoryAppointmentRepository()
+    const sut = new CreateAppointment(appointmentRepository)
 
     const startsAt = getFutureDate('2022-08-10')
     const endsAt = getFutureDate('2022-08-11')
@@ -15,5 +17,40 @@ describe('Create Appointment', () => {
       startsAt,
       endsAt
     })).resolves.toBeInstanceOf(Appointment)
+  })
+
+  it('should not be able to create an appointment with overlapping date', async () => {
+    const appointmentRepository = new InMemoryAppointmentRepository()
+    const sut = new CreateAppointment(appointmentRepository)
+
+    await sut.execute({
+      customer: 'John Doe',
+      startsAt: getFutureDate('2022-08-10'),
+      endsAt: getFutureDate('2022-08-15')
+    })
+
+    expect(sut.execute({
+      customer: 'John Doe',
+      startsAt: getFutureDate('2022-08-14'),
+      endsAt: getFutureDate('2022-08-18')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(sut.execute({
+      customer: 'John Doe',
+      startsAt: getFutureDate('2022-08-08'),
+      endsAt: getFutureDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(sut.execute({
+      customer: 'John Doe',
+      startsAt: getFutureDate('2022-08-08'),
+      endsAt: getFutureDate('2022-08-17')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(sut.execute({
+      customer: 'John Doe',
+      startsAt: getFutureDate('2022-08-11'),
+      endsAt: getFutureDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
   })
 })
--- lives/test-apps-node/src/use-cases/appointment/create-appointment.ts
+++ lives/test-apps-node/src/use-cases/appointment/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from '../../entities/appointment/appointment'
+import { AppointmentRepository } from '../../repositories/appointments-repository'
 
 interface CreateAppointmentRequest {
   customer: string
@@ -9,16 +10,28 @@ interface CreateAppointmentRequest {
 type CreateAppointmentResponse = Appointment
 
 export class CreateAppointment {
+  constructor (
+    private appointmentsRepository: AppointmentRepository
+  ) {}
+
   async execute ({
     customer,
     startsAt,
     endsAt
   }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlappingAppointment = await this.appointmentsRepository.findOverlappingAppointment(startsAt, endsAt)
+
+    if (overlappingAppointment) {
+      throw new Error('Another appointment overlaps this appointment dates')
+    }
+
     const appointment = new Appointment({
       customer,
       startsAt,
       endsAt
     })
+
+    await this.appointmentsRepository.create(appointment)
 
     return appointment
   }
