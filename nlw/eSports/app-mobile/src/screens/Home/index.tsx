51ad93b857da8f2ad29379e6c7f88d90f828b4d4
cortelucas <corte1994.lc@gmail.com>
2022-09-12 22:16:32 -0300

    feat: utilize heading

--- nlw/eSports/app-mobile/src/screens/Home/index.tsx
+++ nlw/eSports/app-mobile/src/screens/Home/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
+
+import logo from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image
+        source={logo}
+        style={styles.logo}
+      />
 
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar ..."
+      />
     </View>
   );
 }
