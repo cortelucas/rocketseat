151cb7c6c3e7de073a5a64ce141a0869a12571b1
cortelucas <corte1994.lc@gmail.com>
2022-09-14 00:32:48 -0300

    feat: implement list ads by game

--- nlw/eSports/server/src/server.ts
+++ nlw/eSports/server/src/server.ts
@@ -27,6 +27,37 @@ server.get('/games', async (request: Request, response: Response) => {
   return response.json(games)
 })
 
+server.get('/games/:id/ads', async (request: Request, response: Response) => {
+  const gameId = request.params.id
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true
+    },
+    where: {
+      gameId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd)
+    }
+  }))
+})
+
 
 
 server.listen(3333, () => console.log('Server is running in port 3333'))
