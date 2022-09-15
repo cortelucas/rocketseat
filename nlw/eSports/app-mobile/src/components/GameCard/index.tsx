d088da46e56636420be35ab8b11d4617cec2a332
cortelucas <corte1994.lc@gmail.com>
2022-09-15 09:53:05 -0300

    feat: implement and style GameCard component

--- nlw/eSports/app-mobile/src/components/GameCard/index.tsx
+++ nlw/eSports/app-mobile/src/components/GameCard/index.tsx
@@ -1,14 +1,16 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ImageBackground, ImageSourcePropType, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 import { THEME } from '../../theme';
 
 import { styles } from './styles';
 
 export interface GameCardProps {
   id: string
-  name: string
-  ads: string
-  cover: ImageSourcePropType
+  title: string
+  _count: {
+    ads: number
+  }
+  bannerUrl: string
 }
 
 interface GCProps extends TouchableOpacityProps {
@@ -19,19 +21,19 @@ export function GameCard({ data, ...rest }: GCProps) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
       <ImageBackground
-        style={styles.cover}
-        source={data.cover}
+        style={styles.bannerUrl}
+        source={{uri: data.bannerUrl}}
       >
 
         <LinearGradient
           colors={THEME.COLORS.FOOTER}
           style={styles.footer}
         >
-          <Text style={styles.name}>
-            {data.name}
+          <Text style={styles.title}>
+            {data.title}
           </Text>
           <Text style={styles.ads}>
-            {data.ads} anúncios
+            { data._count.ads <= 1 ? `${data._count.ads} anúncio` : `${data._count.ads} anúncios` }
           </Text>
         </LinearGradient>
       </ImageBackground>
