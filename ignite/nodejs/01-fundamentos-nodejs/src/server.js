2d02ceecd1b97b851bd94a0e047ce9eef484ec93
Lucas S. Corte <corte1994.lc@gmail.com>
2023-09-09 22:03:04 -0300

    feat: implementign route

--- ignite/nodejs/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,7 +1,37 @@
+import { randomUUID } from 'node:crypto'
 import { createServer } from 'node:http'
 
+const users = []
+
 const server = createServer((request, response) => {
-    return response.end('Olá!')
+  const { method, url } = request
+  try {
+    if (method === 'GET' && url === '/users') {
+      return response
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+    if (method === 'POST' && url === '/users') {
+      users.push({
+        id: randomUUID(),
+        name: 'John Doe',
+        email: 'john.doe@gmail.com'
+      })
+
+      return response
+        .setHeader('Content-type', 'application/json')
+        .writeHead(201)
+        .end(JSON.stringify({
+          message: `Usuário ${users[users.length - 1].id} criado com sucesso.`,
+          user: users[users.length - 1]
+        }))
+    }
+    return response
+      .writeHead(404)
+      .end('Not Found!')
+  } catch (error) {
+    throw new Error(`ERROR: ${error}`)
+  }
 })
 
-server.listen(3333, () => console.log('🚀 Server is running'))
+server.listen(3333, () => console.log('🚀 Server is running on http://localhost:3333'))
