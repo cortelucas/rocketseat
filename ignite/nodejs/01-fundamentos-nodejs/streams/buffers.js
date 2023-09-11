0f7cd99b645aa9561646a01630f8256e1ceaea3c
Lucas S. Corte <corte1994.lc@gmail.com>
2023-09-11 12:35:20 -0300

    entendendo json

---
+++ ignite/nodejs/01-fundamentos-nodejs/streams/buffers.js
@@ -0,0 +1,14 @@
+const buffer = Buffer.from('Hello Buffer!')
+
+console.log(buffer)
+/*
+    output: <Buffer 48 65 6c 6c 6f 20 42 75 66 66 65 72 21>
+    json: {
+            type: 'Buffer',
+            data: [
+                72, 101, 108, 108, 111,
+                32,  66, 117, 102, 102,
+                101, 114,  33
+            ]
+        }
+*/
