35d77ee4244fdd09c98e503ace01f4af25e27dd7
Lucas S. Corte <corte1994.lc@gmail.com>
2023-09-11 12:40:05 -0300

    entendendo middlewares

--- ignite/nodejs/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs/01-fundamentos-nodejs/src/server.js
@@ -1,46 +1,42 @@
 import { randomUUID } from 'node:crypto'
 import { createServer } from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
 const server = createServer(async (request, response) => {
   const { method, url } = request
-  const buffers = []
 
-  for await (const chunk of request) {
-    buffers.push(chunk)
-  }
+  await json(request, response)
 
   try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    request.body = null
-  }
+    if (method === 'GET' && url === '/users') {
+      return response
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+    if (method === 'POST' && url === '/users') {
+      const { name, email } = request.body
+      users.push({
+        id: randomUUID(),
+        name,
+        email
+      })
 
-  if (method === 'GET' && url === '/users') {
+      return response
+        .setHeader('Content-type', 'application/json')
+        .writeHead(201)
+        .end(JSON.stringify({
+          message: `Usuário ${users[users.length - 1].id} criado com sucesso.`,
+          user: users[users.length - 1]
+        }))
+    }
     return response
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+      .writeHead(404)
+      .end('Not Found!')
+  } catch (error) {
+    console.error(`ERROR: ${new Error(error)}`)
   }
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = request.body
-    users.push({
-      id: randomUUID(),
-      name,
-      email
-    })
-
-    return response
-      .setHeader('Content-type', 'application/json')
-      .writeHead(201)
-      .end(JSON.stringify({
-        message: `Usuário ${users[users.length - 1].id} criado com sucesso.`,
-        user: users[users.length - 1]
-      }))
-  }
-  return response
-    .writeHead(404)
-    .end('Not Found!')
 })
 
 server.listen(3333, () => console.log('🚀 Server is running on http://localhost:3333'))
