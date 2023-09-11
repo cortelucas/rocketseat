16926829abcb3f29b9a2634b58b4a385ffce67d9
Lucas S. Corte <corte1994.lc@gmail.com>
2023-09-11 16:43:53 -0300

    persistindo dados

--- ignite/nodejs/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs/01-fundamentos-nodejs/src/server.js
@@ -10,37 +10,39 @@ const server = createServer(async (request, response) => {
 
   await json(request, response)
 
-  try {
-    if (method === 'GET' && url === '/users') {
-      const users = db.select('users')
-      return response
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
-    }
-    if (method === 'POST' && url === '/users') {
-      const { name, email } = request.body
-      const user = {
-        id: randomUUID(),
-        name,
-        email
-      }
+  // try {
 
-      db.insert('users', user)
+  // } catch (error) {
+  //   throw new Error(error)
+  // }
 
-      return response
-        .setHeader('Content-type', 'application/json')
-        .writeHead(201)
-        .end(JSON.stringify({
-          message: `Usuário ${user.id} criado com sucesso.`,
-          user
-        }))
-    }
+  if (method === 'GET' && url === '/users') {
+    const users = db.select('users')
     return response
-      .writeHead(404)
-      .end('Not Found!')
-  } catch (error) {
-    console.error(`ERROR: ${new Error(error)}`)
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = request.body
+    const user = {
+      id: randomUUID(),
+      name,
+      email
+    }
+
+    db.insert('users', user)
+
+    return response
+      .setHeader('Content-type', 'application/json')
+      .writeHead(201)
+      .end(JSON.stringify({
+        message: `Usuário ${user.id} criado com sucesso.`,
+        user
+      }))
+  }
+  return response
+    .writeHead(404)
+    .end('Not Found!')
 })
 
 server.listen(3333, () => console.log('🚀 Server is running on http://localhost:3333'))
