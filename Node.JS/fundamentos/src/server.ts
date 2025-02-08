2b5876725c1e2e055010219bb363e267faadcdbe
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2025-02-08 14:00:55 -0300

    Aula 03: criando e entendendo os middlewares

--- Node.JS/fundamentos/src/server.ts
+++ Node.JS/fundamentos/src/server.ts
@@ -1,5 +1,6 @@
 import { createServer } from "node:http";
 import { randomUUID } from "node:crypto";
+import { json } from "./middlewares/json.ts"
 
 interface User {
   id: string;
@@ -10,24 +11,13 @@ interface User {
 const users: User[] = [];
 
 const server = createServer(async (request, response) => {
-  const buffers: Buffer[] = [];
-  for await (const chunk of request) {
-    buffers.push(chunk);
-  }
-
-  try {
-    request.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    request.body = null;
-  }
-
   const { method, url } = request;
+
+  await json(request, response)
 
   if (method === "GET" && url === "/users") {
     return response
-      .writeHead(200, {
-        "Content-Type": "application/json",
-      })
+      .writeHead(200)
       .end(JSON.stringify(users));
   }
 
@@ -40,15 +30,17 @@ const server = createServer(async (request, response) => {
     };
     users.push(newUser);
 
-    return response.writeHead(201, { "Content-Type": "application/json" }).end(
-      JSON.stringify({
-        message: `Usuário ${newUser.id} - ${newUser.name} criado com sucesso.`,
-      })
-    );
+    return response
+      .writeHead(201)
+      .end(
+        JSON.stringify({
+          message: `Usuário ${newUser.id} - ${newUser.name} criado com sucesso.`,
+        })
+      );
   }
 
   return response
-    .writeHead(404, { "Content-Type": "application/json" })
+    .writeHead(404)
     .end(JSON.stringify({ message: "Not Found" }));
 });
 
