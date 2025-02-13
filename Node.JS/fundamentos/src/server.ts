9dca38e389e93921e5b800a4c9db74df945ad885
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2025-02-13 11:55:23 -0300

    Aula 05: desenvolvendo funções para capturar os query params

--- Node.JS/fundamentos/src/server.ts
+++ Node.JS/fundamentos/src/server.ts
@@ -1,6 +1,7 @@
 import { createServer } from "node:http"
 import { json } from "./middlewares/json.ts"
 import { routes } from "./routes.ts"
+import { extractQueryParams } from "./utils/extract-query-params.ts"
 
 const server = createServer(async (request, response) => {
   const { method, url } = request
@@ -8,13 +9,15 @@ const server = createServer(async (request, response) => {
   await json(request, response)
 
   const route = routes.find(route => {
-      
     return route.method === method && route.path.test(url ?? "/")
   })
 
   if (route) {
     const routeParams = request.url?.match(route.path)
-    request.params = { ...routeParams?.groups }
+    const { query, ...params } = routeParams?.groups ?? {}
+
+    request.params = params
+    request.query = query ? extractQueryParams(routeParams.groups.query) : {}
     
     return route.handler(request, response)
   }
