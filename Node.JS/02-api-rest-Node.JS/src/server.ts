a8fbc2dd737e5aa03904660f41b8550cb13ee5b0
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2025-02-19 14:23:45 -0300

    config: adicionando e configurando knex

--- Node.JS/02-api-rest-Node.JS/src/server.ts
+++ Node.JS/02-api-rest-Node.JS/src/server.ts
@@ -1,12 +1,22 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
-const server = fastify({
-	logger: true,
+const server = fastify()
+
+server.get('/health-check', async (request, reply) => {
+	const tables = await knex('sqlite_schema').select('*')
+
+	return tables
 })
 
-server
-	.listen({
-		port: 3000,
-		host: '0.0.0.0',
-	})
-	.then(address => console.log(`Server listening on ${address}`))
+try {
+	server
+		.listen({
+			port: 3000,
+			host: '0.0.0.0',
+		})
+		.then(address => console.log(`Server listening on ${address}`))
+} catch (error) {
+	server.log.error(error)
+	process.exit(1)
+}
