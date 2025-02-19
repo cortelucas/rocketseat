65effa0f540f2c65ede6a352c60a13b2ee23599f
Lucas dos Santos Corte <corte1994.lc@gmail.com>
2025-02-19 12:32:09 -0300

    config: adicionando ao projeto o biome linter

--- Node.JS/02-api-rest-Node.JS/src/server.ts
+++ Node.JS/02-api-rest-Node.JS/src/server.ts
@@ -1,12 +1,12 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const server = fastify({
 	logger: true,
-});
+})
 
 server
 	.listen({
 		port: 3000,
-		host: "0.0.0.0",
+		host: '0.0.0.0',
 	})
-	.then(() => console.log("Server running on http://localhost:3000"));
+	.then(address => console.log(`Server listening on ${address}`))
